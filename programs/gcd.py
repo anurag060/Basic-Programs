fbc992db75de8ab6cc92ed6cbb3f1da40430a874
anurag <mantripragadaanurag@gmail.com>
2020-10-05 19:22:10 +0530

    changes in GCD code

--- programs/gcd.py
+++ programs/gcd.py
@@ -1,4 +1,4 @@
-def compute_hcf(x, y):
+def compute_gcd(x, y):
 
 # choose the smaller number
     if x > y:
@@ -7,10 +7,10 @@ def compute_hcf(x, y):
         smaller = x
     for i in range(1, smaller+1):
         if((x % i == 0) and (y % i == 0)):
-            hcf = i 
-    return hcf
+            gcd = i 
+    return gcd
 
-num1 = 54 
-num2 = 24
+num1 = int(input("enter first number "))
+num2 = int(input("enter second number "))
 
-print("The H.C.F. is", compute_hcf(num1, num2))
+print("The G.C.D. is", compute_gcd(num1, num2))
